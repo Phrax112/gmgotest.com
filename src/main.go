391cc2fb85c5417d40354006fa09ce8bf9603a08
Gene Moynihan <gmoynihan@protonmail.com>
2020-09-20 11:32:34 +0000

    add contact page

--- src/main.go
+++ src/main.go
@@ -7,8 +7,12 @@ import (
 
 func handlerFunc (w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "text/html")
-    fmt.Println("someone visited the website", r)
-    fmt.Fprint(w, "<h1>Welcome to my awesomish site!</h1>")
+    fmt.Println("someone visited the website\n", r)
+    if r.URL.Path == "/" {
+        fmt.Fprint(w, "<h1>Welcome to my awesomish site!</h1>")
+    } else if r.URL.Path == "/contact" {
+        fmt.Fprint(w, "Send an email to <a href=\"mailto:support@gmgotest.com\">support@gmgotest.com</a>.")
+    }
 }
 
 func main () {
