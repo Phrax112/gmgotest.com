df176ebb6b38c2e22d269c477ee5adbbde95716e
Gene Moynihan <gmoynihan@protonmail.com>
2020-09-20 11:41:20 +0000

    add 4040

--- src/main.go
+++ src/main.go
@@ -12,6 +12,9 @@ func handlerFunc (w http.ResponseWriter, r *http.Request) {
         fmt.Fprint(w, "<h1>Welcome to my awesomish site!</h1>")
     } else if r.URL.Path == "/contact" {
         fmt.Fprint(w, "Send an email to <a href=\"mailto:support@gmgotest.com\">support@gmgotest.com</a>.")
+    } else {
+        w.WriteHeader(http.StatusNotFound)
+        fmt.Fprint(w, "<h1>We could not find that page :(</h1>")
     }
 }
 
